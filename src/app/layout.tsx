5e436e416053a87fd2bf833e4cd8b88cf308929a
TheJustas228 <minecraft.justas29@gmail.com>
2025-11-14 02:46:18 +0100

    Vercel Speed Insights fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const manrope = Manrope({
@@ -37,6 +38,7 @@ export default function RootLayout({
         className={`${manrope.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
